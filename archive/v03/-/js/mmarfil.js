ffd50ffb430e805b29722d0a4119c480c55708e7
Marcelo Marfil <github@mmarfil.com>
2023-11-02 17:32:04 +0000

    new site

--- archive/v03/-/js/mmarfil.js
+++
@@ -1,7 +0,0 @@
-
-// Checks the received 
-function isEmailValid(email) {
-	var pattern = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-	var emailVal = $("#" + email).val();
-	return pattern.test(emailVal);
-}
